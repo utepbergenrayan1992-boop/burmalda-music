6ac7e9138eff44b07c168dd80723d7e006d7ca8f
utepbergenrayan1992-boop <utepbergenrayan1992@gmail.com>
2026-07-25 17:33:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 VOICE_CHANNEL_ID = 1530548841324089354
 
 # Твоя точная ссылка со скриншота Яндекс Диска
-YANDEX_DISK_URL = "https://yandex.kz"
+YANDEX_DISK_URL = "https://disk.yandex.kz/d/pchRD7P7IxItMg"
 
 intents = discord.Intents.default()
 intents.message_content = True
